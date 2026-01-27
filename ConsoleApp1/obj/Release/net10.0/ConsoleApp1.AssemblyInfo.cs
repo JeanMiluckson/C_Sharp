df42be485f7325f99d149a9f7b84138de349eb7a
JEAN MILUCKSON ETIENNE MILIUS <jeanmilius334@gmail.com>
2026-01-27 19:20:30 -0300

    N M

--- ConsoleApp1/obj/Release/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dddd34522483f19985aad19cd13f53f91c0338d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
