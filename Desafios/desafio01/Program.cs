b6a6a7d470ab8573192e3e740926d884b8580fdc
JEAN MILUCKSON ETIENNE MILIUS <jeanmilius334@gmail.com>
2026-02-03 16:52:27 -0300

    add desafio01

---
+++ Desafios/desafio01/Program.cs
@@ -0,0 +1,18 @@
+﻿/*  Declare uma variavel do tipo string chamada jogoFavorito.
+    Declare uma variavel do tipo int chamada horasJogadas.
+    Use Console.WriteLine() para exibir: "Eu joguei [jogoFavorito] por [horasJogadas] horas
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string jogoFavorito = "lol";
+        int horasJogadas = 50;    
+
+        Console.WriteLine($"Eu joguei {jogoFavorito} por {horasJogadas} horas");
+    }    
+
+}
