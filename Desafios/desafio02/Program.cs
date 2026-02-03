a2b52b5619cb1d36ab3e6a0cabf33408a29c94f2
JEAN MILUCKSON ETIENNE MILIUS <jeanmilius334@gmail.com>
2026-02-03 18:08:25 -0300

    desafio02

---
+++ Desafios/desafio02/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    1 - Crie um pequeno script em C# que pergunte ao usuario o nome de um item que ele encontrou no jogo
+    2 - Pergunte a quantidade desse item que ele possui
+    3 - Converta essa quantidade para int
+    4 - Exiba uma mensagem final: "Voce adicionou[quantidade] x [item] ao seu inventario!"
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nome do item: ");
+        string item = Console.ReadLine();
+        Console.Write("quantidade: ");
+        string quantidade = Console.ReadLine();
+        int n_quantidade = Convert.ToInt32(quantidade);
+
+        Console.WriteLine($"Voce adicionou {quantidade}x {item} ao seu iventario!");
+    }
+}
