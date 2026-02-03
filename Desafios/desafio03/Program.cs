63c5659e7fc066d1ed58baed3c56ff320a9c3b52
JEAN MILUCKSON ETIENNE MILIUS <jeanmilius334@gmail.com>
2026-02-03 19:44:25 -0300

    Add desafio03,04

---
+++ Desafios/desafio03/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    1 - Crie uma classe chamada Item
+    2 - Dentro dela, coloque dois campos publicos: string Nome e int Dano
+    3 - No seu Main, instancie(crie) um novo objeto dessa classe
+    4 - Atribua um nome e um valor de dano a esse objeto
+    5 - Exiba o nome do item acessando a propriedade do objeto
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+       Item item = new Item();
+        item.Nome = "Espada";
+        item.Dano = 60;
+        Console.WriteLine($"Item: {item.Nome} | Dano: {item.Dano}");
+    }
+    class Item
+    {
+        public string Nome;
+        public int Dano;
+    }
+}
