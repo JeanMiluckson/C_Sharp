37e17b83de6e844f7da63dd0e5251d1b80ed847f
JEAN MILUCKSON ETIENNE MILIUS <jeanmilius334@gmail.com>
2026-02-03 16:55:09 -0300

    add

--- Projeto01/obj/Debug/net10.0/Projeto01.AssemblyInfo.cs
+++ Projeto01/obj/Debug/net10.0/Projeto01.AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1fb4255cde826c72a580eca150ea030eca34c04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
