ad6692d12c7a75063fe94a39078cfdab9f22e519
JEAN MILUCKSON ETIENNE MILIUS <jeanmilius334@gmail.com>
2026-02-04 18:46:47 -0300

    Add desafio05,06

---
+++ Desafios/desafio05/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    1 - No topo do seu codigo, adicione: using System.Collections.Generic;
+    2 - No seu Main, crie uma lista chamada inventario: List<Item> inventario = new List<Item>();
+    3 - Adicione o seu item e o seu item2 a essa lista usando o metodo .Add()
+    4 - Use um laço foreach para percorrer a lista e chamar o metodo .ExibirStatus() de cada item dentro dela
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+
+class Program
+{
+    static void Main()
+    {
+        List<Item> inventario = new List<Item>();
+        Item item1 = new Item();
+        Item item2 = new Item();
+        item1.Nome = "faca";
+        item1.Dano = 60;
+        item2.Nome = "arco";
+        item2.Dano = 50;
+        inventario.Add(item1);
+        inventario.Add(item2);
+
+        foreach (Item i in inventario)
+        {
+            i.ExibirStatus();
+        }
+    }
+    class Item
+    {
+       public string Nome;
+       public int Dano;
+       public void ExibirStatus()
+        {
+            Console.WriteLine($"Item: {Nome} | Dano: {Dano}");
+        }
+    }
+}
---
+++ Desafios/desafio06/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+    1 - Altere a classe Item para incluir um Construtor que receba dois parametros: string nome e int dano
+    2 - Dentro do construtor, atribua esses parametros aos campos da classe(this.Nome = nome;)
+    3 - No seu Main, apague as linhas onde voce definia o nome e o dano manualmente
+    4 - Agora, crie os itens passando os valores direto no paranteses new Item("faca", 60);
+    5 - Mantenha a lista e o foreach para verificar se tudo continua funcionando
+*/
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<Item> inventario = new List<Item>();
+        Item item1 = new Item("faca", 60);
+        Item item2 = new Item("arco", 50);
+        inventario.Add(item1);
+        inventario.Add(item2);
+        
+        foreach (Item i in inventario)
+        {
+            i.ExibirStatus();
+        }
+    }
+    class Item
+    {
+        public string Nome;
+        public int Dano;
+        public Item(string nome, int dano)
+        {
+            this.Nome = nome;
+            this.Dano = dano;
+        }
+        public void ExibirStatus()
+        {
+            Console.WriteLine($"Item: {Nome} | Dano: {Dano}");
+        }
+        
+    }
+}
